c5da6fdd1576b67da70633fb7d0458c304ab1227
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2025-10-21 11:34:03 +0200

    feat: use gop

--- deepcopy.go
+++ deepcopy.go
@@ -2,7 +2,8 @@ package contentful
 
 import (
 	"bytes"
-	"encoding/json"
+	"encoding/gob"
+	"errors"
 	"sync"
 )
 
@@ -10,16 +11,33 @@ var buffers = sync.Pool{
 	New: func() interface{} { return new(bytes.Buffer) },
 }
 
-func DeepCopy[S, T any](dst *T, src S) error {
-	buf := buffers.Get().(*bytes.Buffer)
-	buf.Reset()
-	defer buffers.Put(buf)
+func DeepCopy[T any](dst *T, src T) error {
+	buf, ok := buffers.Get().(*bytes.Buffer)
+	if !ok {
+		return errors.New("could not get buffer from pool")
+	}
+	defer func() {
+		buf.Reset()
+		buffers.Put(buf)
+	}()
 
-	enc := json.NewEncoder(buf)
-	if err := enc.Encode(src); err != nil {
+	if err := gob.NewEncoder(buf).Encode(src); err != nil {
 		return err
 	}
 
-	dec := json.NewDecoder(buf)
-	return dec.Decode(dst)
+	return gob.NewDecoder(buf).Decode(dst)
 }
+
+// func DeepCopy[S, T any](dst *T, src S) error {
+// 	buf := buffers.Get().(*bytes.Buffer)
+// 	buf.Reset()
+// 	defer buffers.Put(buf)
+//
+// 	enc := json.NewEncoder(buf)
+// 	if err := enc.Encode(src); err != nil {
+// 		return err
+// 	}
+//
+// 	dec := json.NewDecoder(buf)
+// 	return dec.Decode(dst)
+// }
