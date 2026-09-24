03a7bf9bbaed00c2b591c0c6edda6ed88cd58bce
Cristian Vidmar <cristian.vidmar@gmail.com>
2026-09-24 10:27:10 +0200

    fix: close the response body when there is nothing to decode

--- contentful.go
+++ contentful.go
@@ -228,14 +228,12 @@ func (c *Contentful) do(req *http.Request, v any) error {
 	}
 
 	if res.StatusCode >= 200 && res.StatusCode < 400 {
+		defer res.Body.Close()
 		if v != nil {
-			defer res.Body.Close()
-			err = json.NewDecoder(res.Body).Decode(v)
-			if err != nil {
+			if err := json.NewDecoder(res.Body).Decode(v); err != nil {
 				return err
 			}
 		}
-
 		return nil
 	}
 
